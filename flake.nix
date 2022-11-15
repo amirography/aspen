403f7bd08abd8a8e1c18e85e88866d089c3cfcf8
amirography <amirhossein.alesheikh@gmail.com>
2022-11-15 14:09:05 +0330

    build(): rebuild flake

--- flake.nix
+++ flake.nix
@@ -1,45 +1,80 @@
 {
   description = "nixos helper for this nixos user"
   ;
+
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk.url = "github:nmattia/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, nixpkgs, naersk }:
+    let
+      cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      overlay = final: prev: {
+        "${cargoToml.package.name}" = final.callPackage ./. { inherit naersk; };
+      };
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
-
-        naersk' = pkgs.callPackage naersk { };
-        buildInputs = with pkgs; [
-          openssl
-        ];
-
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-      in
-      {
-
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-          pname = "nixme";
-          version = "0.1.0";
-
-          inherit buildInputs nativeBuildInputs;
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlay
+            ];
+          };
+        in
+        {
+          "${cargoToml.package.name}" = pkgs."${cargoToml.package.name}";
+        });
 
 
-        };
+      defaultPackage = forAllSystems (system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      })."${cargoToml.package.name}");
 
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs;
-        };
-      }
-    );
+      checks = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlay
+            ];
+          };
+        in
+        {
+          format = pkgs.runCommand "check-format"
+            {
+              buildInputs = with pkgs; [ rustfmt cargo ];
+            } ''
+            ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
+            ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
+            touch $out # it worked!
+          '';
+          "${cargoToml.package.name}" = pkgs."${cargoToml.package.name}";
+        });
+      devShell = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          };
+        in
+        pkgs.mkShell {
+          inputsFrom = with pkgs; [
+            pkgs."${cargoToml.package.name}"
+          ];
+          buildInputs = with pkgs; [
+            rustfmt
+            nixpkgs-fmt
+          ];
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        });
+    };
 }
+
--- src/commands.rs
+++ src/commands.rs
@@ -4,41 +4,56 @@ use super::config;
 
 pub fn commands_runner(conf: &config::Config, c: CommandOption) -> Result<(), CommandRepoErr> {
     match c {
-        CommandOption::Swtich => Command::new("nixos-rebuild")
-            .args(vec![
-                "switch",
-                "--flake",
-                &format!(
-                    "{}#",
-                    conf.get_nix_flake_path()
-                        .to_str()
-                        .ok_or(CommandRepoErr::ErrorInMakingString)?
-                ),
-            ])
-            .stdout(std::process::Stdio::piped())
-            // .stderr(std::process::Stdio::piped())
-            .spawn()
-            .map_err(|e| CommandRepoErr::ErrorRunningCommad(e.to_string()))?
-            .wait()
-            .map_err(|e| CommandRepoErr::ErrorWhileWaiting(e.to_string()))?,
+        CommandOption::Swtich => {
+            let child = Command::new("nixos-rebuild")
+                .args(vec![
+                    "switch",
+                    "--flake",
+                    &format!(
+                        "{}#",
+                        conf.get_nix_flake_path()
+                            .to_str()
+                            .ok_or(CommandRepoErr::ErrorInMakingString)?
+                    ),
+                ])
+                .stdout(std::process::Stdio::piped())
+                .stderr(std::process::Stdio::piped())
+                .output()
+                .map_err(|e| CommandRepoErr::ErrorRunningCommad(e.to_string()))?;
 
-        CommandOption::Update => Command::new("nix")
-            .args(vec![
-                "flake",
-                "update",
-                &format!(
-                    "{}",
-                    conf.get_nix_flake_path()
-                        .to_str()
-                        .ok_or(CommandRepoErr::ErrorInMakingString)?
-                ),
-            ])
-            .stdout(std::process::Stdio::piped())
-            // .stderr(std::process::Stdio::piped())
-            .spawn()
-            .map_err(|e| CommandRepoErr::ErrorRunningCommad(e.to_string()))?
-            .wait()
-            .map_err(|e| CommandRepoErr::ErrorWhileWaiting(e.to_string()))?,
+            let e = String::from_utf8_lossy(&child.stderr);
+            println!("{}", e);
+
+            if !child.status.success() {
+                return Err(CommandRepoErr::NonZeroStatus);
+            };
+        }
+
+        CommandOption::Update => {
+            let mut child = Command::new("nix")
+                .args(vec![
+                    "flake",
+                    "update",
+                    &format!(
+                        "{}",
+                        conf.get_nix_flake_path()
+                            .to_str()
+                            .ok_or(CommandRepoErr::ErrorInMakingString)?
+                    ),
+                ])
+                .stdout(std::process::Stdio::piped())
+                .stderr(std::process::Stdio::piped())
+                .spawn()
+                .map_err(|e| CommandRepoErr::ErrorRunningCommad(e.to_string()))?;
+
+            let st = child
+                .wait()
+                .map_err(|e| CommandRepoErr::ErrorWhileWaiting(e.to_string()))?;
+
+            if !st.success() {
+                return Err(CommandRepoErr::NonZeroStatus);
+            };
+        }
     };
 
     Ok(())
@@ -62,4 +77,7 @@ pub enum CommandRepoErr {
 
     #[error("could not run command")]
     ErrorWhileWaiting(String),
+
+    #[error("Command exited with non-0 value")]
+    NonZeroStatus,
 }
